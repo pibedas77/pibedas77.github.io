c7f4a52169cdd817f634f8ff5d7adad9a1bb1e06
Mattia Pivetta <mattia.pivetta@gmail.com>
2021-01-26 22:00:22 +0100

    Wednesday: Elif statements

--- Week_03.py
+++ Week_03.py
@@ -120,3 +120,62 @@ if ans < 10:
     print(result)
 else:
     print("Your number is greater than 9")
+
+### Tuesday exercises ###
+## Elif Statements
+
+x, y = 10, 10
+if x < y:
+    print("y is greater")
+elif x > y:
+    print("x is greater")
+elif x == y:
+    print("x is equal to y")
+
+x, y, z = 10, 10, 11
+if x < y:
+    print("y is greater")
+elif x > y:
+    print("x is greater")
+elif x == y:
+    if x == z:
+        print("x is equal to y, but also to z")
+    elif x != z:
+        print("x is equal to y, but NOT to z")
+
+# A mayor difference between Elif statements against multiple If statements is:
+# Elif is connected to one If statement, so only one condition is true and the rest do not run
+# The following lines print both outputs
+x, y, z = 10, 10, 11
+if x == y:
+    print("x is equal to y")
+if x < z:
+    print("z is greater than x")
+
+# The following lines return only one output
+# Because once one of the condition returns True the rest is not evaluated
+x, y, z = 10, 10, 11
+if x == y:
+    print("x is equal to y")
+elif x < z:
+    print("z is greater than x")
+
+## Wednesday exercise
+# 1.
+ans = int(input("Give me a number and I'll compare it with the number 100"))
+if ans > 100:
+    print("Your input is greater than 100")
+elif ans < 100:
+    print("Your input is smaller than 100")
+else:
+    print("Your input is equal to 100")
+
+# 2.
+
+x, y = 5, 10
+if x > y:
+    print("greater")
+elif x < y:
+    print("lower")
+
+# 3.

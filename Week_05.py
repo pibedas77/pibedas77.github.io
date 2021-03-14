232063d7d43327980f8c770d829dc24d7638bd35
Mattia Pivetta <mattia.pivetta@gmail.com>
2021-03-14 22:28:29 +0100

    Monday: creating and calling functions

---
+++ Week_05.py
@@ -0,0 +1,19 @@
+## Functions
+# Creating and calling functions
+
+# Monday: creating and calling functions
+def printInfo():
+    print("Name: Mattia")
+    print("Age: 33")
+
+
+printInfo()
+printInfo()
+
+
+def calc():
+    x, y = 10, 15
+    print(x + y)
+
+
+calc()

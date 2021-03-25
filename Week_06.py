1b368ca8349a1f053e0347b6fff5a847972d3308
Mattia Pivetta <mattia.pivetta@gmail.com>
2021-03-25 21:48:07 +0100

    Tuesday: working with dictionaries

--- Week_06.py
+++ Week_06.py
@@ -47,3 +47,50 @@ pizza = {"ingridients": ["cheese", "sausage", "peppers"]}
 
 for i in pizza["ingridients"]:
     print(i)
+
+# Tuesday: working with dictionaries
+
+car = {"year": "2018"}
+# Adding a key
+car["color"] = "Blue"
+
+print("Year: {} \t Color: {}".format(car["year"], car["color"]))
+
+# Changing a value
+car["color"] = "Violet"
+
+print("Year: {} \t Color: {}".format(car["year"], car["color"]))
+
+# deleting a key
+try:
+    del car["year"]
+except:
+    print("The dictionary key was not fund")
+
+print(car)
+
+# Looping a dictionary
+# Looping keys
+person = {"name": "Mattia", "age": "33"}
+for key in person.keys():
+    print(key)
+
+# Looping on values
+person = {"name": "Mattia", "age": "33"}
+for value in person.values():
+    print(value)
+
+# Looping Key-value Pairs
+person = {"name": "Mattia", "age": "33"}
+for key, value in person.items():
+    print("{} and {}".format(key, value))
+
+# User Input
+person = {"name": "", "address": "", "number": ""}
+person["name"] = input("Hi, what is your name?")
+person["address"] = input("What is your address?")
+person["number"] = input("What is your number?")
+
+print("These are de details you gave...")
+for v in person.values():
+    print(v)
